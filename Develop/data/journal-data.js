2ac250c8650cf5d765a217c16bd25290a1674be8
Randall Phillips <rphill45@uncc.edu>
2019-11-20 23:11:37 -0500

    added final jquery and routes

--- Develop/data/journal-data.js
+++ Develop/data/journal-data.js
@@ -1,16 +1,11 @@
 var journalArray = [
 
-    {"title":"tests",
-    "body":"asfdfsd",
-    "journal":"food"},
+    {title:"tests",
+    body:"asfdfsd",
+    journal:"food"},
 
-    {"title":"lskfjsdlk"
-    ,"body":"sdjlfkjdsklf",
-    "journal":"excercise"},
     
-    {"title":"ouch",
-    "body":"dude",
-    "journal":"food"}
+
 ];
 
 module.exports = journalArray;
--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -3,5 +3,9 @@ var journalData = require('../data/journal-data.js');
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
         res.json(journalData);
+    });
+
+    app.post('api/notes', (req, res) => {
+        journalData.push(req.body);
     })
 }
