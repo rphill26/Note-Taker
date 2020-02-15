1384217c8c3e2e067cf35cb2d1d242d835b3dc0e
Randall Phillips <rphill45@uncc.edu>
2020-02-15 13:00:21 -0500

    fixed server issue and routing

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./Develop/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const db = require("./db.json");
 
-require('./Develop/routing/html-routes.js')(app);
-require('./Develop/routing/api-routes.js')(app);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+
+    return res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  db.push({
+    id: db.length > 0 ? db[db.length - 1].id + 1 : 1,
+    ...newNote
+  });
+  fs.writeFile(path.join(__dirname, "./db.json"), JSON.stringify(db), err => {
+    if (err) throw err;
+    res.json(db);
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.writeFile(
+    path.join(__dirname, "./db.json"),
+    JSON.stringify(db.filter(note => note.id !== parseInt(req.params.id))),
+    err => {
+      if (err) throw err;
+      // res.json(db)
+      res.send("Deleted");
+    }
+  );
+});
 
 app.listen(PORT, () => {
-    console.log('Listening on PORT: ' + PORT);
+  console.log(`Server listening at http://localhost:${PORT}/`);
 });
